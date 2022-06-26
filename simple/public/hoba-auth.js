56c00883c00655acc3aafa389e5699524616cd61
Eric Willisson <epw@google.com>
2022-06-26 03:52:52 +0000

    Prompt for login with redirect

--- simple/public/hoba-auth.js
+++ simple/public/hoba-auth.js
@@ -1,11 +1,18 @@
 const HOBA_AUTH_VALUES = {
     FAILED: "AUTH FAILED",
+    LOGIN: "/login.html",
 };
 
 class HobaAuth {
     check_user(text) {
 	if (text.startsWith(HOBA_AUTH_VALUES.FAILED)) {
-	    alert("Not logged in.");
+	    const redirect = confirm("Not logged in. Press OK to go to login page, or Cancel to remain here.");
+	    if (redirect) {
+		const r = new URL(location);
+		r.pathname = HOBA_AUTH_VALUES.LOGIN;
+		r.searchParams.set("redirect", location);
+		location = r;
+	    }
 	    return null;
 	}
 	return true;
