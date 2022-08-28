9cc918635a4e0903647f889af4fc9c3ef96b080e
Eric Willisson <epw@google.com>
2022-08-28 06:00:27 +0000

    Clearer instructions

--- simple/public/hoba.js
+++ simple/public/hoba.js
@@ -102,7 +102,7 @@ const HOBA_UI = `
  </div>
  <div id="hoba-nothing" class="hoba-ui-row">
   <p>
-   To log in on this device, open <a id="hoba-login-link" href="">this page</a> on a device where you already can log in, click the "Link to Log In Elsewhere" button, and then: &bull; enter the provided code here, &bull; scan the QR code, or &bull; visit the provided URL.
+   To log in on this device, open <a id="hoba-login-link" href="">this page</a> on a device where you already can log in, click the "Link to Log In Elsewhere" button, and then on this device: &bull; enter the provided code below, &bull; scan the QR code, or &bull; visit the provided URL.
   </p>
   <p>
    <input type="number" id="hoba-share-code-entry" onchange="HOBA.enter_share_code(event)">
